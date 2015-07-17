713ab6edb4b9fa95c806e846ff5a6c8f5c229a33
87maza <87maza@gmail.com>
2015-07-16 19:33:47 -0700

    mongolab

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://john:john@apollo.modulusmongo.net:27017/ubU8texe',
+	//'database': 'mongodb://john:john@apollo.modulusmongo.net:27017/ubU8texe',
+	'database': 'mongodb://john:john@ds045882.mongolab.com:45882/hackrfarm',
 	//'database': 'mongodb://localhost:27017/test',
 	 'superSecret': 'ilovescotchscotchyscotchscotch'
 //http://localhost:8080/auth/linkedin/callback
