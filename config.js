02564fd0dceb254b20c66cc05b0e54ac0bdefa97
87maza <87maza@gmail.com>
2015-07-16 19:54:50 -0700

    heroku please

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
 	//'database': 'mongodb://john:john@apollo.modulusmongo.net:27017/ubU8texe',
-	'database': 'mongodb://john:john@ds045882.mongolab.com:45882/hackrfarm',
-	//'database': 'mongodb://localhost:27017/test',
+	//'database': 'mongodb://john:john@ds045882.mongolab.com:45882/hackrfarm',
+	'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
 	 'superSecret': 'ilovescotchscotchyscotchscotch'
 //http://localhost:8080/auth/linkedin/callback
 	//mongodb://<user>:<pass>@apollo.modulusmongo.net:27017/ubU8texe
